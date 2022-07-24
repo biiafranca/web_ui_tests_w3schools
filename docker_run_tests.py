4b17a50ce66a3c58c66472b05d135fbcbbc6ad41
bi053344 <bianca.inacio@intelbras.com.br>
2022-07-24 17:39:55 -0300

    change image name

--- docker_run_tests.py
+++ docker_run_tests.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 import subprocess
 
-subprocess.run('docker build -t run_robot ./', shell=True, capture_output=True)
+subprocess.run('docker build -t run_web_tests ./', shell=True, capture_output=True)
 
 local_path = Path().absolute()
 tests_path = local_path.joinpath("tests")
 results_path = local_path.joinpath("results")
 resources_path = local_path.joinpath("resources")
 
-subprocess.run(f'docker run --rm --name executa_robot'
+subprocess.run(f'docker run --rm --name run_robot_tests'
                f' -v "{tests_path}":/opt/robotframework/tests'
                f' -v "{results_path}":/opt/robotframework/results'
                f' -v "{resources_path}":/opt/robotframework/resources'
-               f' run_robot:latest robot -d /opt/robotframework/results /opt/robotframework/tests',
+               f' run_web_tests:latest robot -d /opt/robotframework/results /opt/robotframework/tests',
                shell=True, capture_output=False)
